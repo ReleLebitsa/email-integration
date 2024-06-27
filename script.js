b3a442a9b008b38d5d89f1b9fe0e1c0947f7b592
TessaLehong <136630270+TessaLehong@users.noreply.github.com>
2024-06-27 02:03:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,4 +6,27 @@ $( '.js-input' ).keyup(function() {
     }
   });
 
+<script>
+			document
+				.getElementById("contact-form")
+				.addEventListener("submit", function (event) {
+					event.preventDefault();
+					emailjs.sendForm("service_umjc10x", "template_4p81ygf", this).then(
+						function (response) {
+							console.log("Email sent!", response.status, response.text);
+							document.getElementById("email_id").value = "";
+							document.getElementById("animal").value = "";
+							document.getElementById("size").value = "";
+							document.getElementById("date").value = "";
+							document.getElementById("package").value = "";
+							document.getElementById("message").value = "";
 
+							alert("Email sent successfully!");
+						},
+						function (error) {
+							console.error("Error sending email:", error);
+							
+						}
+					);
+				});
+		</script>
